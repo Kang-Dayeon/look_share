2e6ec3534651de347ab542daa1d0d5f21531cad2
kangDayeon <dksldiss89@gmail.com>
2022-07-09 00:17:37 +0900

    회원가입 페이지 작업

--- lookshare/src/components/signUp.vue
+++ lookshare/src/components/signUp.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="sing" v-if="tab == 3">
-    <div class="sing__wrap">
-        <form class="sing__form" action="./signUp">
-            <div class="input__box">
-                <label for="name">Name</label>
-                <input id="name" type="text" placeholder="홍길동">
-            </div>
-            <div class="input__box">
-                <label for="id">Id</label>
-                <input id="id" type="text" placeholder="영문 대소문자를 조합하여 8자 이하로 만들어주세요" maxlength="8">
-            </div>
-            <div class="input__box">
-                <label for="pw">Password</label>
-                <input id="pw" type="password">
-            </div>
-        </form>
-    </div>
+    <form class="sing__form" action="./signUp">
+        <legend class="title">Sign Up</legend>
+        <div class="input__box">
+            <label for="name">Name</label>
+            <input id="name" type="text" placeholder="홍길동">
+        </div>
+        <div class="input__box">
+            <label for="id">Id</label>
+            <input id="id" type="text" placeholder="영문 대소문자를 조합하여 8자 이하로 만들어주세요" maxlength="8">
+        </div>
+        <div class="input__box">
+            <label for="pw">Password</label>
+            <input id="pw" type="password">
+        </div>
+        <input class="btn" type="submit" value="SignUp">
+    </form>
   </div>
 </template>
 
