2f64bca9572365d6b915b88b6eb5a34bb6c2ce85
kangdayeon <dksldiss89@gmail.com>
2022-07-11 15:17:26 +0900

    다시 셋팅

--- lookshare/src/main.js
+++ lookshare/src/main.js
@@ -1,10 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import axios from "axios";
 
 //fort-awesome
 import { library } from '@fortawesome/fontawesome-svg-core'
-// import { faGratipay } from '@fortawesome/free-brands-svg-icons'
+
 import { faHouse } from '@fortawesome/free-solid-svg-icons'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
@@ -17,8 +16,6 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 library.add(faHouse, faPlus, faUser, faChevronLeft, faChevronRight, faCheck, faMessage)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-
-Vue.prototype.$http = axios;
 
 Vue.config.productionTip = false
 
--- lookshare/vue.config.js
+++ lookshare/vue.config.js
@@ -1,17 +1,4 @@
-// const { dirname } = require('path')
-const path = require('path')
-module.exports = {
-  devServer: {
-    proxy: {
-      //proxy 요청을 보낼 api 시작 부분
-      '/api': {
-        target: 'http://localhost:3000/api',
-        changeOrigin: true,
-        pathRewite: {
-          '^/api': ''
-        }
-      }
-    },
-  },
-  outputDir: path.resolve(__dirname, '../backend/public')
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
